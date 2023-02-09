896c6065a72cb9f26cf6a625749827aa3fcbb511
Alessandro Baccini <baccini.al@gmail.com>
2023-02-09 12:44:37 +0100

    updated docs style

--- docs/make.bat
+++ docs/make.bat
@@ -1,4 +1,4 @@
-@ECHO OFF
+
 
 pushd %~dp0
 
@@ -12,8 +12,7 @@ set BUILDDIR=build
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
-	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+	echo.	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
 	echo.installed, then set the SPHINXBUILD environment variable to point
 	echo.to the full path of the 'sphinx-build' executable. Alternatively you
 	echo.may add the Sphinx directory to PATH.
