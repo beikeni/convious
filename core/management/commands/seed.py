0d636da73299fe80d7d90cb419389a3d6c572b9a
Alessandro Baccini <baccini.al@gmail.com>
2023-02-09 15:36:35 +0100

    typo in seed script

--- core/management/commands/seed.py
+++ core/management/commands/seed.py
@@ -10,12 +10,13 @@ from core.models import Restaurant, IndividualVote, DAILY_VOTE_LIMIT, Vote
 def populate_past_votes():
     start_date = datetime.datetime.strptime('2023-01-01', "%Y-%m-%d").date()
     days = datetime.date.today() - start_date
+    restaurants = [restaurant.id for restaurant in Restaurant.objects.all()]
     for index in range(days.days):
         current_date = start_date + datetime.timedelta(days=index)
         vote = Vote.objects.create(date=current_date)
         for user in get_user_model().objects.all():
             for _ in range(DAILY_VOTE_LIMIT - 1):
-                user_vote(user=user.id, restaurant=random.randint(1, 10), vote=vote)
+                user_vote(user=user.id, restaurant=random.choice(restaurants), vote=vote)
 
 
 def user_vote(user, restaurant, vote):
