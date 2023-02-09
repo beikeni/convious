84baa2b7b90db215d42cba47e76f2a5ca03cfc95
Alessandro Baccini <baccini.al@gmail.com>
2023-02-09 11:24:44 +0100

    added gitignore, removed sqlite

--- convious/settings.py
+++ convious/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bb&3go_j%(%=x65h3=s*r^kxp%smns4*j=7l()%0y=2plx@j_q"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
